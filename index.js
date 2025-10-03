c63dc40a86d8cafe9c79a31dfde81f4d7cd2874e
coderman982 <khanfuzail955@gmail.com>
2025-10-03 08:38:04 +0530

    u

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ app.get("/about",(req,res)=>{
 
 app.listen(3000);
 console.log("http://localhost:3000/");
+//nodemon is used to automatically restart the server when file changes in the directory are detected.
+//To install nodemon use the command npm install -g nodemon
+//To run the server using nodemon use the command nodemon index.js
+//or npm run server if the script is defined in package.json
