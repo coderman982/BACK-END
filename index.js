85d0dbefc82747b5069da15ea5e7f12496ec0c66
coderman982 <khanfuzail955@gmail.com>
2025-10-03 09:41:20 +0530

    jk

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express=require('express');
 const app=express();
+const mongoose=require('mongoose');
+
+
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
@@ -11,9 +14,14 @@ app.get("/about",(req,res)=>{
 
 })
 
+mongoose.connect("mongodb+srv://admin:Ru7AX9BsQcytdrf4@cluster0.sy0jyov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+    console.log("Connected to MongoDB");
+    app.listen(3000);
+    console.log("http://localhost:3000/");
+}).catch((err)=>{
+    console.log("Error connecting to MongoDB",err);
+});
 
-app.listen(3000);
-console.log("http://localhost:3000/");
 //nodemon is used to automatically restart the server when file changes in the directory are detected.
 //To install nodemon use the command npm install -g nodemon
 //To run the server using nodemon use the command nodemon index.js
