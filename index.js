ec4beda9f17a3795a5cb6f5f5151555c28b6bc09
coderman982 <khanfuzail955@gmail.com>
2025-10-04 19:55:51 +0530

    f

--- index.js
+++ index.js
@@ -10,15 +10,10 @@ app.get("/",(req,res)=>{
 })
 
 
-app.post("/api/products", async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
-//connect to mongodb
+app.post("/api/products",(req,res)=>{
+    const product=new Product(req.body);
+    res.status(201).json(product);
+})//connect to mongodb
 
 
 
