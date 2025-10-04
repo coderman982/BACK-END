932020543e7e7e527403e9eae0d30e4225ca1a4f
coderman982 <khanfuzail955@gmail.com>
2025-10-04 19:53:14 +0530

    d

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ app.get("/",(req,res)=>{
     res.send("Hello World");
 })
 
-app.get("/about",(req,res)=>{
-
-    res.send("<h1>About Page</h1>");
-
-})
 
 app.post("/api/products", async (req, res) => {
     try {
