604ab6f68e316011a1b7519f77cd70954dda1922
coderman982 <khanfuzail955@gmail.com>
2025-10-18 20:00:35 +0530

    sd

--- index.js
+++ index.js
@@ -35,3 +35,6 @@ mongoose.connect("mongodb+srv://admin:gplEmrfaT5A2hY9k@cluster0.jjd91os.mongodb.
 
 
 
+//nodemon is used to automatically restart the server when file changes in the directory are detected.
+//To install nodemon use the command npm install -g nodemon
+//To run the server using nodemon use the command nodemon index.js
