1bead0729534dedf9a0e40b1b3736ea0f9de7f7e
coderman982 <khanfuzail955@gmail.com>
2025-10-18 20:01:48 +0530

    re

--- models/product.model.js
+++ models/product.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const productschema = new mongoose.Schema({
     name: { type: String, required: true },
-    price: { type: Number,  default: 0 },
+    price: { type: Number, required: true, default: 0 },
     image: { type: String }
 }, { timestamps: true }); // timestamps will automatically add createdAt and updatedAt
 
