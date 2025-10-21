252d217ba86a863d6901d88a423d8c9f3a8914a7
coderman982 <khanfuzail955@gmail.com>
2025-10-21 23:04:03 +0530

    kjn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post("/api/products", async (req, res) => {
     }
 }); 
 
-//the api
+//the 
 app.get("/api/products/:id", async(req,res)=>{
     try{
         const id=req.params;// get the id from the request parameters it is for accessing a specific product by id 
