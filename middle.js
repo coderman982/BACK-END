2e9daf3c7afee999bac2bb4ea1d85daa70c01292
coderman982 <khanfuzail955@gmail.com>
2025-10-24 18:26:25 +0530

    mjb

--- middle.js
+++ middle.js
@@ -6,13 +6,29 @@ const app = Express();
 app.use(Express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', middle, (req, res) => {
+app.use(middle);
+
+app.get('/',  (req, res) => {
   res.send('Hello World!');
 });
 
+
+app.get('/about', middle2, (req, res) => {
+  res.send('About Page');});//first middleware function will be executed here then when we go to /about route second middleware function will be executed
+         //then /about route handler will be executed
 
 
 function middle(req,res,next) {
   console.log('This is a middleware function');
   next();
 }
+
+
+function middle2(req,res,next) {
+  console.log('This is another middleware function');
+  next();
+}
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
