02838e4c7aca07270a4a4842b128d2cf95a69e69
coderman982 <khanfuzail955@gmail.com>
2025-10-24 18:24:27 +0530

    nn

--- middle.js
+++ middle.js
@@ -6,6 +6,12 @@ const app = Express();
 app.use(Express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', middle, (req, res) => {
+  res.send('Hello World!');
+});
+
+
+
 function middle(req,res,next) {
   console.log('This is a middleware function');
   next();
